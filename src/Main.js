25841684e5797b8250db348ede9ee5ad66cfb401
And4Web <anands2807@gmail.com>
2022-01-20 19:17:34 -0800

    latout created

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class Main extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
     <div>
-      <h1>The Main Component.</h1>
+      <h1>React Counter</h1>
+      <h1>0</h1>
+      <button className="btn btn-danger">DECREMENT</button>
+      <button className="btn btn-primary">NEAUTRAL</button>
+      <button className="btn btn-success">INCREMENT</button>
     </div>
     );
   }
