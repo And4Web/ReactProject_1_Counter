712ff845c041073fc683e208687494c1a3bffb1c
And4Web <anands2807@gmail.com>
2022-01-20 20:03:39 -0800

    css completed

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class Main extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
     return (
-    <div className="container">
-      <h1>React Counter</h1>
-      <h1>0</h1>
-      <button className="btn btn-danger">DECREMENT</button>
-      <button className="btn btn-primary">NEUTRAL</button>
-      <button className="btn btn-success">INCREMENT</button>
-    </div>
+      <div className="row justify-content-center">
+        <div className="col-md-5 container">
+          <h1>React Counter</h1>
+          <h1>0</h1>
+          <button className="btn btn-danger">DECREMENT</button>
+          <button className="btn btn-primary">NEUTRAL</button>
+          <button className="btn btn-success">INCREMENT</button>
+        </div>
+      </div>
     );
   }
 }
